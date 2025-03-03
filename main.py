8575cbe4b0b363f6c46f26f1c84c57871068ccbe
elfffffy <124286592+elfffffy@users.noreply.github.com>
2025-03-03 22:36:59 +0900

    refactor: add error handle

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from typing import Untion
+from typing import Untion, Optional
 
-def add(x: Untion[int, float], y: Untion[int, float]) => Untion[int, float]
+def add(x: Untion[int, float], y: Untion[int, float]) -> Optional[Untion[int, float]]:
+    try: 
+        result = x + y
+    except Exception as e:
+            print(e)
+            return None
+
+    return result    
 
 if __name__ == "--main__":
     print(add(1, 2))
